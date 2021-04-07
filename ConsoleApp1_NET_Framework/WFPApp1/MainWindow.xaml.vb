04e6784eb8d00abb35f9c4cba29f61d0343eb20f
Mark Roenfeldt <1187productions@gmail.com>
2021-04-07 15:41:16 -0600

    Added wpf apps for VB practice projects

---
+++ ConsoleApp1_NET_Framework/WFPApp1/MainWindow.xaml.vb
@@ -0,0 +1,17 @@
+﻿Class MainWindow
+    Private Sub TextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
+
+    End Sub
+
+    Private Sub btn_Click(sender As Object, e As RoutedEventArgs) Handles btn.Click
+        txtBlock1.Text = DateTime.Now.DayOfWeek.ToString
+    End Sub
+
+    'Private Sub txtBox1_TextChanged(sender As Object, e As TextChangedEventArgs) Handles txtBox1.TextChanged
+    '    txtBlock1.Text = txtBox1.Text
+    'End Sub
+
+    Private Sub txtBox1_TouchEnter(sender As Object, e As TouchEventArgs) Handles txtBox1.TouchEnter
+
+    End Sub
+End Class
